988c3cb3565f52eae0505872328cd714f058a365
jackofdiamond5 <jackofdiamond596@gmail.com>
2019-06-25 13:37:08 +0300

    chore(app.component): add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+// if you'd like to see the lazy loading in action, you need to run the project locally
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
